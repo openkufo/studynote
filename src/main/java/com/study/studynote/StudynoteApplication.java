4dc1ff80d0dae5ebd2d49912ace2dc9bb004b6ff
paul Han <openkufo@gmail.com>
2023-02-08 16:04:23 +0900

    추가 : studynote 초기화

---
+++ src/main/java/com/study/studynote/StudynoteApplication.java
@@ -0,0 +1,15 @@
+package com.study.studynote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import lombok.Getter;
+
+@SpringBootApplication
+public class StudynoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudynoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/studynote/StudynoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.studynote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudynoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
