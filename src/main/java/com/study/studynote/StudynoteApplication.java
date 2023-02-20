03e08b92834a21e8afa4ebbda4d5a8e9cbc34101
paul Han <openkufo@gmail.com>
2023-02-20 13:33:06 +0900

    TEST

--- src/main/java/com/study/studynote/StudynoteApplication.java
+++ src/main/java/com/study/studynote/StudynoteApplication.java
@@ -3,7 +3,6 @@ package com.study.studynote;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import lombok.Getter;
 
 @SpringBootApplication
 public class StudynoteApplication {
